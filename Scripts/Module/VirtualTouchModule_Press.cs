85dd16626206cdd5ea17f3c18ada8caf6bd887c8
Isshi-777 <Isshi@777>
2021-05-22 21:19:50 +0900

    [add] 「押した際」のモジュールを作成。定義クラスの作成

---
+++ Scripts/Module/VirtualTouchModule_Press.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+/// <summary>
+/// 「タッチ（押した際）」モジュール
+/// </summary>
+[DisallowMultipleComponent]
+public class VirtualTouchModule_Press : AVirtualTouchMojule_OneFinger
+{
+    /// <summary>
+    /// イベント
+    /// </summary>
+    public delegate void OnPressEvent();
+    public OnPressEvent OnPress { set; get; }
+
+    public override VirtualTouchPadConstants.ModuleType ModuleType => VirtualTouchPadConstants.ModuleType.Press;
+
+    protected override void OnTouchDown()
+    {
+        this.OnPress?.Invoke();
+    }
+}
---
+++ Scripts/VirtualTouchPadConstants.cs
@@ -0,0 +1,16 @@
+﻿/// <summary>
+/// VertualTouchPadの定数定義クラス
+/// </summary>
+public static class VirtualTouchPadConstants
+{
+    /// <summary>
+    /// モジュールタイプ
+    /// </summary>
+    public enum ModuleType
+    {
+        /// <summary>
+        /// 押した際
+        /// </summary>
+        Press,
+    }
+}
