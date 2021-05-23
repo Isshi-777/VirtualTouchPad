df8193f56ff8efbdddd0c86920ac1a82cd7f5f16
Isshi-777 <Isshi@777>
2021-05-23 12:43:06 +0900

    [add] サンプル作成

---
+++ Samples~/Example/VirtualTouchPadExample.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace Isshi777
+{
+    /// <summary>
+    /// VirtualTouchPadのサンプル
+    /// </summary>
+    public class VirtualTouchPadExample : MonoBehaviour
+    {
+        [SerializeField]
+        private VIrtualTouchPad touchPad;
+
+        private void Start()
+        {
+            {// Press
+                VirtualTouchModule_Press module;
+                if (this.touchPad.TryGetModule<VirtualTouchModule_Press>(VirtualTouchPadConstants.ModuleType.Press, out module))
+                {
+                    module.OnPress += () => Debug.Log(" Press !! ");
+                }
+            }
+
+            {// LongPress
+                VirtualTouchModule_LongPress module;
+                if (this.touchPad.TryGetModule<VirtualTouchModule_LongPress>(VirtualTouchPadConstants.ModuleType.LongPress, out module))
+                {
+                    module.OnLongPress += () => Debug.Log(" LongPress !! ");
+                }
+            }
+
+            {// Click
+                VirtualTouchModule_Click module;
+                if (this.touchPad.TryGetModule<VirtualTouchModule_Click>(VirtualTouchPadConstants.ModuleType.Click, out module))
+                {
+                    module.OnClick += () => Debug.Log(" Click !! ");
+                }
+            }
+
+            {// Drag
+                VirtualTouchModule_Drag module;
+                if (this.touchPad.TryGetModule<VirtualTouchModule_Drag>(VirtualTouchPadConstants.ModuleType.Drag, out module))
+                {
+                    module.OnDrag += (direction) => Debug.Log(" Drag !! " + direction);
+                }
+            }
+
+            {// Flick
+                VirtualTouchModule_Flick module;
+                if (this.touchPad.TryGetModule<VirtualTouchModule_Flick>(VirtualTouchPadConstants.ModuleType.Flick, out module))
+                {
+                    module.OnFlick += (directionType) => Debug.Log(" Flick !! " + directionType);
+                }
+            }
+        }
+    }
+}
